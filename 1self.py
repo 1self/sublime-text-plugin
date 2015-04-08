38e9b10d65a4270529740fcc12831eb44bc0849b
Douglas Vaz <douglas1992@gmail.com>
2015-04-08 16:03:26 +0530

    Cleanup

--- 1self.py
+++ 1self.py
@@ -11,14 +11,14 @@ import os
 import sys
 
 ST_VERSION = int(sublime.version())
-PLUGIN_VERSION = 'v0.0.13'
+PLUGIN_VERSION = 'v0.0.14'
 QD_URL = "http://app.1self.co" # keep this http instead of https so that it works on ubuntu and other OS where https is not supported for python. "urllib.error.URLError: <urlopen error unknown url type: https>"
 AUTHORIZAION = {
     'app-id': 'app-id-598358b6aacda229634d443c9539662b',
     'app-secret': 'app-secret-782411ad58934863f63545ccc180e407ffbe66cf5e9e02d31c2647ea786ead33'
 }
 SETTINGS_FILE = "1self.sublime-settings"
-package_name = "sublime-text-plugin"
+package_name = "1​Self"
 runThread = True
 
 try:
